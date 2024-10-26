51d41cda2f0477020f04775c5bb120646a1f7aa1
Dan M <dsmiller95@gmail.com>
2024-10-26 12:59:11 -0500

    most simple version of merger repository

---
+++ VerbMerger/Merger/MergerRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VerbMerger.Merger;
+
+public record MergeInput(string Subject, string Verb, string Object);
+
+public record MergeOutput(string Word, PartOfSpeech PartOfSpeech);
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum PartOfSpeech
+{
+    Verb,
+    Noun
+}
+
+public interface IMergerRepository
+{
+    public Task<MergeOutput> GetOutput(MergeInput input);
+}
+
+public class MergerRepository : IMergerRepository
+{
+    public async Task<MergeOutput> GetOutput(MergeInput input)
+    {
+        var rng = new Random();
+        return rng.Next(0, 3) switch
+        {
+            0 => new MergeOutput(input.Subject, PartOfSpeech.Noun),
+            1 => new MergeOutput(input.Verb, PartOfSpeech.Verb),
+            2 => new MergeOutput(input.Object, PartOfSpeech.Noun),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
--- VerbMerger/Program.cs
+++ VerbMerger/Program.cs
@@ -1,9 +1,12 @@
+using VerbMerger.Merger;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IMergerRepository, MergerRepository>();
 
 var app = builder.Build();
 
@@ -34,6 +37,14 @@ app.MapGet("/weatherforecast", () =>
         return forecast;
     })
     .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.MapGet("/merge/{subject}/{verb}/{object}", async (string subject, string verb, string @object, IMergerRepository repository) =>
+    {
+        var output = await repository.GetOutput(new MergeInput(subject, verb, @object));
+        return output;
+    })
+    .WithName("Merge")
     .WithOpenApi();
 
 app.Run();
