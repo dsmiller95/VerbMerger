50605e44ffd52527765df8ccce57c7cee62684cf
Dan M <dsmiller95@gmail.com>
2024-11-13 19:43:35 -0600

    onlyu create mongodb when we run in development

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,16 +1,19 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
-
-var mongo = builder.AddMongoDB("mongo")
-    // withDataVolume ensures data persists across restarts
-    //.WithDataBindMount("mongo-merge-results")
-    .WithDataVolume("mongo-merge-results")
-    ;
-
-var mongodb = mongo.AddDatabase("mongodb", "verb_merger");
 
 var apiService = builder
     .AddProject<Projects.VerbMerger>("apiservice")
-    .WithReference(mongodb)
     .WithExternalHttpEndpoints();
+if (builder.Environment.IsDevelopment())
+{
+    var mongo = builder.AddMongoDB("mongo")
+        // withDataVolume ensures data persists across restarts
+        //.WithDataBindMount("mongo-merge-results")
+        .WithDataVolume("mongo-merge-results")
+        ;
+    var mongodb = mongo.AddDatabase("mongodb", "verb_merger");
+    apiService = apiService.WithReference(mongodb);
+}
 
 builder.Build().Run();
